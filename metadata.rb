6fb615872f69fdf1cd9125787b6bd59bdcfc53a7
Nick Gray <f0rkz@f0rkznet.net>
2017-09-01 01:07:56 -0400

    Add supported OS

--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,7 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/f0rkz/chef-steamcmd/issues'
 source_url 'https://github.com/f0rkz/chef-steamcmd'
 depends 'tar'
+
+%w(ubuntu debian).each do |os|
+  supports os
+end
