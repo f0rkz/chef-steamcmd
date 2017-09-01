dede94e138a8bf902ce3981481c68ed0f68125ec
Nick Gray <f0rkz@f0rkznet.net>
2017-09-01 02:13:28 -0400

    Verson 0.1.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'f0rkz@f0rkznet.net'
 license 'Apache-2.0'
 description 'Installs steamcmd and steamcmd applications'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.4'
+version '0.1.5'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/f0rkz/chef-steamcmd/issues'
 source_url 'https://github.com/f0rkz/chef-steamcmd'
