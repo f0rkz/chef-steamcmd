7ff54ff2bcc956c5431955f4520e5f6ea99fee19
Nick Gray <f0rkz@f0rkznet.net>
2017-09-01 01:12:49 -0400

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name 'steamcmd'
 maintainer 'Nick Gray'
 maintainer_email 'f0rkz@f0rkznet.net'
 license 'Apache-2.0'
-description 'Installs/Configures steamcmd'
-long_description 'Installs/Configures steamcmd'
-version '0.1.0'
+description 'Installs steamcmd and steamcmd applications'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/f0rkz/chef-steamcmd/issues'
 source_url 'https://github.com/f0rkz/chef-steamcmd'
