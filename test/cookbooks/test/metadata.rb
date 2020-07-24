8583ec7dd772609750e0c165b12f46964b1b066d
Shaun Mouton <sdmouton@devops.christmas>
2020-07-24 15:44:33 -0500

    update readme and bump version
    Signed-off-by: Shaun Mouton <sdmouton@devops.christmas>

--- test/cookbooks/test/metadata.rb
+++ test/cookbooks/test/metadata.rb
@@ -3,6 +3,6 @@ maintainer 'Nick Gray'
 maintainer_email 'f0rkz@f0rkznet.net'
 license 'Apache-2.0'
 description 'Installs/Configures test'
-version '0.2.1'
+version '0.3.1'
 
 depends 'chef-steamcmd'
